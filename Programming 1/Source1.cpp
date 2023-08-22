5b29efbac42b2523245ad8a7c0b267352498654a
Mjangel1 <marcelo.mjextreme@gmail.com>
2023-08-22 13:21:40 +0200

    compulsory1

--- Programming 1/Source1.cpp
+++ Programming 1/Source1.cpp
@@ -9,3 +9,10 @@ int main()
 	std::cout << "VIRUS DETECTED DELETING ALL FILES!";
 	return 0;
 }
+
+
+// int main is a function and what inside the {} is what we want to execute.
+// ; - end the code
+// return 0 end the function
+// std is what we use to reference using namespace in the code.
+// #include get the iostream file.
