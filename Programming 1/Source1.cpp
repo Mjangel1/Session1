a779254d381aae1034923957debe946ca84e6f8c
Mjangel1 <marcelo.mjextreme@gmail.com>
2023-08-22 12:05:55 +0200

    conversation version

--- Programming 1/Source1.cpp
+++ Programming 1/Source1.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 
- int main() {
- 
-	 
-	std::cout << "Hello world!";
+int main()
+{
 
+	std::cout << "Hello world!\n";
+	std::cout << "How are you!\n";
+	std::cout << "VIRUS DETECTED DELETING ALL FILES!";
 	return 0;
+}
---
+++ Programming 1/Source2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	std::cin >> information;
+	std::getline(std::cin, variable);
+	std::cout << variable;
+	return 0;
+}
